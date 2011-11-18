1e9dd6c306bcb4d4def3914464e49f13769d9954
roofis0 <roofis20002003@yahoo.com>
2011-11-18 12:33:44 -0500

    added a make script for linux and windows

--- make.sh
+++ make.sh
@@ -1,13 +1,24 @@
 cd src
+<<<<<<< HEAD
 javac screencapture/*.java
+=======
+javac screencapture\*.java
+>>>>>>> added a make script for linux and windows
 echo Main-Class: screencapture.ScreenCapture > man.txt
 jar cfm ScreenCapture.jar man.txt screencapture
 rm man.txt
 cd screencapture
 rm *.class
+<<<<<<< HEAD
 cd ../..
 cp src/ScreenCapture.jar .
 rm src/ScreenCapture.jar
 chmod +x ScreenCapture.jar
 echo press any key to exit.
 read w
+=======
+cd ..\..
+cp src\ScreenCapture.jar .
+rm src\ScreenCapture.jar
+pause
+>>>>>>> added a make script for linux and windows
