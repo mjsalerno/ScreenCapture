56d4b0efb23d204e78ee14236692d0661ef39bf9
mike salerno <roofis20002003@yahoo.com>
2011-11-20 20:12:54 -0500

    fixed the make shell script

--- make.sh
+++ make.sh
@@ -1,11 +1,13 @@
 cd src
-javac screencapture\*.java
+javac screencapture/*.java
 echo Main-Class: screencapture.ScreenCapture > man.txt
 jar cfm ScreenCapture.jar man.txt screencapture
 rm man.txt
 cd screencapture
 rm *.class
-cd ..\..
-cp src\ScreenCapture.jar .
-rm src\ScreenCapture.jar
-pause
+cd ../..
+cp src/ScreenCapture.jar .
+rm src/ScreenCapture.jar
+chmod +x ScreenCapture.jar
+echo press any key to exit.
+read w
