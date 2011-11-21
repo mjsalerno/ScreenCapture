6c7a4607176951bed100f1ea404da87b26b16a38
Annuate <paul@p-campbell.com>
2011-11-20 20:11:38 -0500

    Made PicNode serializeable so t can be written to a binary file. Added a binary file writer to the PictureWriterThread looking to enhance file writing performance and turn into .jpg images at a later time.

--- src/screencapture/PicNode.java
+++ src/screencapture/PicNode.java
@@ -1,12 +1,15 @@
 package screencapture;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 /**
  *
  * @author JJ_2011
  */
-public class PicNode 
+public class PicNode implements Serializable
 {    
     private BufferedImage img;
     private String fileName;
@@ -25,5 +28,26 @@ public class PicNode
     public synchronized String getFilePath()
     {
         return this.fileName;
+    }
+    
+    public byte[] getBytes()
+    {
+        byte[] buff = null;
+        
+        try
+        {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream() ;
+            ObjectOutputStream out = new ObjectOutputStream(bos);
+            out.writeObject(this);
+            out.flush();
+            out.close();
+            buff = bos.toByteArray();
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Problem Serializeing Login");
+            ex.printStackTrace();
+        }
+        return buff;
     }
 }
